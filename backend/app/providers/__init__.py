70ff119ddc19f9a17b3eb37139c154f2eb290f91
Athul A L <alathul15@gmail.com>
2026-09-12 09:13:58 +0530

    Fix GitLab provider export for PR #37

--- backend/app/providers/__init__.py
+++ backend/app/providers/__init__.py
@@ -1,5 +1,6 @@
 from app.providers.hibp import HIBPProvider
 from app.providers.gravatar import GravatarProvider
 from app.providers.github import GitHubProvider
+from app.providers.gitlab import GitLabProvider
 from app.providers.public_web import PublicWebProvider
 from app.providers.rdap import RDAPProvider
